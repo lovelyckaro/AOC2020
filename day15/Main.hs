6430483e3649357a7f7e29db7ff310fdcc85fda6
Love Lyckaro <love.lyckaro@hotmail.se>
2020-12-15 07:52:56 +0100

    Clean solution for day 15

--- day15/Main.hs
+++ day15/Main.hs
@@ -1,9 +1,7 @@
-{-# LANGUAGE BangPatterns #-}
 module Main where
 
-import Data.List (elemIndex)
-import Data.IntMap.Lazy (IntMap)
-import qualified Data.IntMap.Lazy as M
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as M
 
 input :: [Int]
 input = [1,20,8,12,0,14]
@@ -15,20 +13,19 @@ data Spoken = Spoken {spoken :: IntMap Int, lastSpoken :: Int, stepsTaken :: Int
     deriving (Show)
 
 emptySpoken :: Spoken
-emptySpoken = Spoken M.empty 0 0 3000000
+emptySpoken = Spoken M.empty 0 0 30000000
 
 fillStart :: [Int] -> Spoken -> Spoken
 fillStart startnums (Spoken _ l st ms) = Spoken (M.fromList (zip (init startnums) [1..])) (last startnums) (length startnums) ms
 
-step :: Spoken -> Spoken
-step (Spoken sp l st ms) = case M.lookup l sp of
-        Just index -> Spoken (M.insert l st sp) (st - index) (st + 1) ms
-        Nothing -> Spoken (M.insert l st sp) 0 (st + 1) ms
-    
-run :: [Int] -> [Int]
-run inits = lastSpoken <$> iterate step (fillStart inits emptySpoken )
+run :: Spoken -> Int
+run (Spoken sp l st ms) = if st >= ms then l else
+    case M.lookup l sp of
+        Just index -> run $ Spoken (M.insert l st sp) (st - index) (st + 1) ms
+        Nothing -> run $ Spoken (M.insert l st sp) 0 (st + 1) ms
 
 
 main :: IO ()
 main = do
-    print $ (run input) !! (30000000 -6)
+    let initSpoken = fillStart input emptySpoken
+    print . run $ initSpoken 

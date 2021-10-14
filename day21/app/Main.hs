b6e31fc4c3c35bdfff98f8bdbd008f5e6f2ce7e2
Love Lyckaro <love.lyckaro@hotmail.se>
2021-10-14 21:17:16 +0200

    Ran ormolu on day 21 lmao

--- day21/app/Main.hs
+++ day21/app/Main.hs
@@ -2,25 +2,31 @@ module Main where
 
 import Data.Map (Map, (\\))
 import qualified Data.Map as M
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import Data.Void ( Void )
 import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Void (Void)
+import Text.Megaparsec
+  ( Parsec,
+    errorBundlePretty,
+    parse,
+    some,
+    (<|>),
+  )
+import Text.Megaparsec.Char (eol, letterChar, space, string)
 
 type Parser = Parsec Void String
 
-newtype Allergen = Allergen { getAllergen :: String}
+newtype Allergen = Allergen {getAllergen :: String}
   deriving (Show, Eq, Ord)
 
-newtype Ingredient = Ingredient { getIngredient :: String}
+newtype Ingredient = Ingredient {getIngredient :: String}
   deriving (Show, Eq, Ord)
 
 data Line = Line {ingredients :: [Ingredient], allergens :: [Allergen]}
-  deriving Show
+  deriving (Show)
 
 toMap :: Line -> Map Allergen (Set Ingredient)
-toMap (Line ins alls) = M.fromList [(allergen, S.fromList ins) | allergen <- alls]
+toMap (Line ins alls) = M.fromList [(allergen, S.fromList ins) | allergen <- alls]
 
 toMaps :: [Line] -> [Map Allergen (Set Ingredient)]
 toMaps = map toMap
@@ -59,23 +65,23 @@ reducePossibles = foldr combine M.empty
     combine = M.unionWith S.intersection
 
 allIngredients :: [Line] -> Map Ingredient Int
-allIngredients xs = foldr (\i m -> M.insertWith (+) i 1 m) M.empty (concatMap ingredients xs )
+allIngredients xs = foldr (\i m -> M.insertWith (+) i 1 m) M.empty (concatMap ingredients xs)
 
 removePossibles :: Map Ingredient Int -> Map Allergen (Set Ingredient) -> Map Ingredient Int
-removePossibles all possibles = all \\ M.fromSet undefined removables
+removePossibles all possibles = all \\ M.fromSet undefined removables
   where
     removables :: Set Ingredient
     removables = foldr S.union S.empty (M.elems possibles)
 
 decideAllergens :: Map Allergen (Set Ingredient) -> Map Allergen Ingredient
 decideAllergens m = helper m M.empty
-  where 
+  where
     helper :: Map Allergen (Set Ingredient) -> Map Allergen Ingredient -> Map Allergen Ingredient
-    helper uncertain known 
+    helper uncertain known
       | M.null uncertain = known
-      | otherwise = helper (removeSingletons uncertain) (addSingletons uncertain known)
+      | otherwise = helper (removeSingletons uncertain) (addSingletons uncertain known)
     removeSingletons :: Map Allergen (Set Ingredient) -> Map Allergen (Set Ingredient)
-    removeSingletons xs = M.map (removeThese (M.elems (singletons xs))) (xs \\ singletons xs)
+    removeSingletons xs = M.map (removeThese (M.elems (singletons xs))) (xs \\ singletons xs)
     addSingletons :: Map Allergen (Set Ingredient) -> Map Allergen Ingredient -> Map Allergen Ingredient
     addSingletons xs known = M.union known (singletons xs)
     singletons :: Map Allergen (Set Ingredient) -> Map Allergen Ingredient
@@ -92,9 +98,9 @@ part1 xs = sum (removePossibles allIns dangerous)
 part2 :: [Line] -> String
 part2 xs = foldr1 (\s1 s2 -> s1 <> "," <> s2) list
   where
-    dangerous = reducePossibles . toMaps $ xs
+    dangerous = reducePossibles . toMaps $ xs
     mapping = decideAllergens dangerous
-    list = map (getIngredient . snd) . M.toAscList $ mapping
+    list = map (getIngredient . snd) . M.toAscList $ mapping
 
 main :: IO ()
 main = do
@@ -104,4 +110,3 @@ main = do
       print (part1 xs)
       print (part2 xs)
     Left error -> putStrLn (errorBundlePretty error)
-
